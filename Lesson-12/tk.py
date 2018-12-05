2c7dbcc49ae4a6829899695a4672c79508c28f43
GIobal <45609012+GIobal@users.noreply.github.com>
2018-12-05 16:03:35 +0100

    Add all exercises of lesson 12.

---
+++ Lesson-12/tk.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+root = Tk() # Creëer het hoofdscherm
+
+button1 = Button(master = root, text = 'Button 1')
+button1.pack(side = LEFT, pady = 10)
+
+button2 = Button(master = root, text = 'Button 2')
+button2.pack(side = RIGHT, pady = 10)
+
+button3 = Button(master = root, text = 'Button 2')
+button3.pack(side = TOP, pady = 10)
+
+root.mainloop()
